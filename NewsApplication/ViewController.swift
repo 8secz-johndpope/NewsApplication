4880226c32e41baa6298bdb36c30e12f0df2069c
VladyslavMaslennykov <maslennykov.v@gmail.com>
2019-07-10 11:57:23 +0300

    Initial Commit

---
+++ NewsApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApplication
+//
+//  Created by Vladyslav on 7/10/19.
+//  Copyright © 2019 Vladyslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
