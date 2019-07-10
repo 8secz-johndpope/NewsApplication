2912019baaa3bc381f1194f1d3c15a78c8c23ecb
VladyslavMaslennykov <maslennykov.v@gmail.com>
2019-07-10 20:32:55 +0300

    tableview created

--- NewsApplication/TableViewForNews/FeedNewsController.swift
+++ NewsApplication/TableViewForNews/FeedNewsController.swift
@@ -12,18 +12,26 @@ import SwiftyJSON
 
 class FeedNewsController: UIViewController {
     
-    fileprivate var arrayOfSources = ["business-insider", "independent", "mtv-news", "rbc", "reuters"]
-
+//    fileprivate var arrayOfSources = ["business-insider", "independent", "mtv-news", "rbc", "reuters"]
+//    var arrayOfTitles: [String] = [] {
+//        didSet {
+//            DispatchQueue.main.async {
+//                //
+//            }
+//        }
+//    }
+//    var arrayOfDescription: [String] = []
+//    var arrayOfImageLinks: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
-    @IBAction func refreshNews(_ sender: Any) {
-        let apiCall = APICall()
-        apiCall.getAPI(sources: arrayOfSources)
-    }
-    
+//    @IBAction func refreshNews(_ sender: Any) {
+//        let apiCall = APICall()
+//        apiCall.getAPI(sources: arrayOfSources)
+//        print(arrayOfImageLinks)
+//    }
 }
 
